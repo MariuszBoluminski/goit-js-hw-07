406ba20bea9063742c7567ead825ed17b7e7b9f0
MariuszBoluminski <mariusz.boluminski@gmail.com>
2023-11-16 21:11:43 +0100

    gallery_git

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+let instance;
+
+const markup = galleryItems
+  .map(
+    (image) => `<li>
+<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>
+</li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target.classList.contains("gallery__image")) {
+    const selectedImage = event.target.dataset.source;
+    instance = basicLightbox.create(
+      `
+  		<img width="1400" height="900" src="${selectedImage}">
+  	`
+    );
+    instance.show();
+
+    document.addEventListener("keydown", closePreview);
+  }
+}
+
+function closePreview(event) {
+  if (event.key == "Escape") {
+    console.log("naciśnięto esc");
+    document.removeEventListener("keydown", closePreview);
+    instance.close();
+  }
+}
+
+gallery.addEventListener("click", handleClick);

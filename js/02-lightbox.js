1d261151be349c1afdeaf521a029ef641b945297
MariuszBoluminski <mariusz.boluminski@gmail.com>
2023-11-17 08:55:07 +0100

    lightbox_git

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (image) => `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>
+</li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+const galleryImages = document.querySelectorAll(".gallery__item");
+
+new SimpleLightbox(galleryImages, {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
